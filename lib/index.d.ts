b7ab23419f1182ecd1087d7f8a8e87f6d96c8fe4
Markiian Linynskyi <markiian.linynskyi@rolique.io>
2018-11-13 13:41:49 +0200

    feat(clean): clean the project

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import * as React from 'react';
-import { ReactFunction } from './utils';
-declare const useTouch: (element: JSX.Element | ReactFunction) => [React.ReactElement<any>, boolean];
-export default useTouch;
--- lib/utils.d.ts
+++
@@ -1,3 +0,0 @@
-import * as React from 'react';
-export declare type ReactFunction = (...arg: any[]) => React.ReactElement<any>;
-export declare const checkTheElement: (element: React.ReactElement<any> | ReactFunction) => element is ReactFunction;
